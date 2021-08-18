daec4f26526af325a65d80563c5d8dabe57ee1f0
Zadigo <pendenquejohn@gmail.com>
2021-08-18 13:12:26 +0200

    Created items for virtualenv

--- full_stack_template/urls.py
+++ full_stack_template/urls.py
@@ -1,13 +1,14 @@
 # from django.contrib import admin
 from accounts.admin import admin_site
 from django.urls import path
-from django.urls.conf import include
+from django.urls.conf import include, re_path
 
-from full_stack_template.views import index
+from full_stack_template.views import index, redirect_view
 
 urlpatterns = [
     path('api/v1/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin_site.urls),
+    # re_path(r'^(?:\w+\/)+$', redirect_view),
     path('', index, name='index'),
 ]
